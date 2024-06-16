b46e9cec3b6d7e7c6054932e039aa73ce463e3e7
JacketCoder <89178522+jacketcoder@users.noreply.github.com>
2024-06-16 18:07:48 +0800

    Update generate.py to latest version

--- phi/generate.py
+++ phi/generate.py
@@ -1,14 +1,23 @@
-import nltk, re, phi.sentiment as sentiment, random
+import nltk, re, phi.sentiment as sentiment, random, phi.basicmath as basicmath
 stop = set(nltk.corpus.stopwords.words("english"))
 lem = nltk.stem.WordNetLemmatizer()
 def now(userinput, subject):
     userinput_new = userinput
+    userinput_new = userinput_new.replace(". ","#")
+    userinput_new = userinput_new.replace(", ","#")
+    userinput_new = userinput_new.replace(": ","#")
+    userinput_new = userinput_new.replace("; ","#")
+    userinput_new = userinput_new.replace("? ","#")
+    userinput_new = userinput_new.replace("! ","#")
     userinput_new = userinput_new.replace(".","#")
     userinput_new = userinput_new.replace(",","#")
     userinput_new = userinput_new.replace(":","#")
     userinput_new = userinput_new.replace(";","#")
     userinput_new = userinput_new.replace("?","#")
     userinput_new = userinput_new.replace("!","#")
+    mathdetect = userinput_new.replace("#", " ")
+    while mathdetect.endswith(" "):
+        mathdetect = mathdetect[:-1]
     usinp = userinput_new.split("#")
     usin = []
     for item in usinp:
@@ -25,7 +34,7 @@ def now(userinput, subject):
     sentence_list = []
     generated_list = []
     if score < -0.25:
-        generated_list.append("Aww, that's bad.")
+        generated_list.append("Oh no, that's bad.")
     elif score >= 0.25:
         generated_list.append("Great!")
     else:
@@ -34,13 +43,11 @@ def now(userinput, subject):
         i = nltk.word_tokenize(sentence)
         i = " ".join([lem.lemmatize(k) for k in i if k.isalpha() and k not in stop])
         sentence_list.append(i)
-        file = open("phi/data.txt","r")
-        content_raw = file.read()
+    file = open("phi/data.txt","r")
+    content_raw = file.read()
     for this in range(0,len(sentence_list)):
         sentence_each = []
-        for l in sentence_list:
-            a = l.upper()
-        matches = re.finditer(a, content_raw)
+        matches = re.finditer(sentence_list[this].upper(), content_raw)
         positions = [match.end() for match in matches if match.group(0).isupper()]
         for that in range(0,len(positions)):
             if content_raw[positions[that] - 1].isalpha() or content_raw[positions[that] + 1].isalpha():
@@ -54,4 +61,9 @@ def now(userinput, subject):
             generated_list.append(random.choice(sentence_each))
     if not generated_list:
         generated_list.append("Okay.")
-    return " ".join(generated_list)
+    if "That would be " + basicmath.returnvalue(mathdetect) + "." == "That would be invalid.":
+        return " ".join(generated_list)
+    elif "That would be " + basicmath.returnvalue(mathdetect) + "." == "That would be decimal.":
+        return "I can't calculate decimals."
+    else:
+        return "That would be " + basicmath.returnvalue(mathdetect) + "."
